5623e51d134f363e22e0e382738cf3a816b1b3a0
Aditya Amol Patil <157286761+Aditya948351@users.noreply.github.com>
2025-04-07 17:05:02 +0000

    Added Button of Start Quiz

--- Quiz-Generator/src/app/page.tsx
+++ Quiz-Generator/src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold">Hello World👋</h1>
+    <div className="flex flex-col flex-1">
+    <main className="flex min-h-screenf justify-center flex-1">
+      <h1 className="text-6xl font-bold">Quiz Generator using pdf app</h1>
     </main>
+    <footer className="footer pb-9 px-6 relative mb-0ff">
+      <Button>Start the Quiz Generation</Button>
+    </footer>
+    </div>
   )
 }
