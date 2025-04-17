e2c042e6081c4c4aab47fe428795730519943459
Aditya Patil <adityapatil9721@gmail.com>
2025-04-17 22:17:36 +0530

    a

--- Quiz-Generator/src/app/quizz/UploadDoc.tsx
+++ Quiz-Generator/src/app/quizz/UploadDoc.tsx
@@ -5,15 +5,20 @@ import { Button } from "@/components/ui/button";
 const UploadDoc = () => {
   const [document, setDocument] = useState< Blob | File | null | undefined>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
 
   const handleSubmit = async (
     e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if(!document) {
+      setError("Please upload the pdf first");
+      return;
+    }
     setIsLoading(true);
     const formData = new FormData();
     formData.append("pdf", document as Blob);
     try{
-      const res = await fetch("/api/quizz/generate", {
+      const res = await fetch("/api/quizz/generate/", {
         method:"POST",
         body:formData
       });
@@ -23,6 +28,7 @@ const UploadDoc = () => {
     } catch(e) {
       console.log("error while generating",e);
     } 
+    setIsLoading(false);
   }
   return (
     <div className="w-full">
@@ -31,14 +37,14 @@ const UploadDoc = () => {
             <label htmlFor="document" 
             className="bg-secondary w-full flex h-20 rounded-md border-4 border-dashed border-blue-900 relative">
                 <div className="absolute inset-0 m-auto flex justify-center items-center">
-                  {document && document?.name? document.
-                  name : "Drag or upload pdf file here"}
-                  </div>
+                  {document && document?.name? document.name : "Drag or upload pdf file here"}
+                </div>
                 <input type="file" id="document" 
                 className="relative block w-full h-full 
                 z-50 opacity-0" onChange={(e) => setDocument
                 (e?.target?.files?.[0])}/>
             </label>
+            {error ? <p className="text-red-600">{error}</p> : null}
             <Button size="lg" className="mt-2" 
             type="submit">Generate Quiz</Button>
         </form>
