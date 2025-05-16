b05f6247a766d90ae04781a6e233c28b7410e3c0
Aditya Amol Patil <157286761+Aditya948351@users.noreply.github.com>
2025-05-16 20:11:03 +0530

    Update drizzle.config.ts

--- Quiz-Generator/drizzle.config.ts
+++ Quiz-Generator/drizzle.config.ts
@@ -6,7 +6,6 @@ export default {
   driver: "pg",
   dbCredentials: {
     connectionString:
-      process.env.DATABASE_URL ||
-      "postgres://postgres:postgres@localhost:5432/postgres",
+      process.env.DATABASE_URL
   },
 } satisfies Config;
