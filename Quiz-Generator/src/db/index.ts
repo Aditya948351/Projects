6e4a6b61e78a02cb3575489d51651487cd76ab4c
Aditya Amol Patil <157286761+Aditya948351@users.noreply.github.com>
2025-05-16 20:09:25 +0530

    Update index.ts

--- Quiz-Generator/src/db/index.ts
+++ Quiz-Generator/src/db/index.ts
@@ -3,8 +3,7 @@ import postgres from "postgres";
 import * as schema from "./schema";
 
 const connectionString =
-  process.env.DATABASE_URL ||
-  "postgres://postgres:postgres@localhost:5432/postgres";
+  process.env.DATABASE_URL;
 
 const client = postgres(connectionString);
 export const db = drizzle(client, { schema });
